db08fed34c1decdbb13e5bb06464f78cde7c849c
Mr-XX23 <starbalami000@gmail.com>
2025-05-25 21:49:07 +0545

    fix: correct formatting and improve structure of app.js

--- app.js
+++ app.js
@@ -12,28 +12,29 @@ import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.disable("x-powered-by");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(arcjetMiddleware);
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
-app.use('/api/v1/workflows', workflowRouter);
+app.get("/", (req, res) => {
+  res.send("Welcome to the Subcriptions Model API");
+});
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/workflows", workflowRouter);
 app.use(errorMiddleware);
- 
+
 app.get("/", (req, res) => {
   res.send("Welcome to the home page");
 });
 
-
-
 app.listen(PORT, async () => {
-    console.log(`Server is running on port ${PORT}`);
-    await connectToDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+  await connectToDB();
+});
 
 export default app;
